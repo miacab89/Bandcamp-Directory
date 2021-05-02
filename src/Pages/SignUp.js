e7c3e610abfa4a4d42d9aa448aea3e9db2835ed5
Mia Cabrera <antemia.kavillari@gmail.com>
2021-05-01 23:34:45 -0500

    signup form

--- src/Pages/SignUp.js
+++ src/Pages/SignUp.js
@@ -1,15 +1,39 @@
-import React, {Component} from 'react';
+import React, {useRef} from 'react';
 import NavBar from '../Components/NavBar/index.js'
+import { Form, Card, Button } from 'react-bootstrap';
 
-export class SignUp extends Component {
-render() {
+export default function SignUp() {
+
+
+    const emailRef = useRef()
+    const passwordRef = useRef()
+    const passwordConfirmRef = useRef() 
     return (
         <div>
-            <NavBar />
-            SIGN UP
+        <NavBar />
+            <Card className="form-card">
+                <Card.Body>
+                    <h2 className="text-center mb-4">SignUp</h2>
+                    <Form>
+                        <Form.Group id="email">
+                            <Form.Label>E-Mail</Form.Label>
+                                <Form.Control className="text-field" id="email" ref={emailRef} required />
+                        </Form.Group>
+                        <Form.Group id="password">
+                            <Form.Label>Password</Form.Label>
+                                <Form.Control className="text-field" id="password" ref={passwordRef} required />
+                        </Form.Group>
+                        <Form.Group id="passwordConfirm">
+                            <Form.Label>Re-Enter Password</Form.Label>
+                                <Form.Control className="text-field" id="passwordConfirm" ref={passwordConfirmRef} required />
+                        </Form.Group>
+                        <Button className="button">Sign Up</Button>
+                    </Form>
+                </Card.Body>
+            </Card>
         </div>
     )
 }
-}
 
-export default SignUp; 
+
+ 
